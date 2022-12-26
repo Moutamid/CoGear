dffa57e12bb834d6614bcafbef0b2820f7f66e09
suleman <sunnybajwa904@gmail.com>
2022-12-26 18:13:02 +0500

    commit

--- app/src/main/java/com/moutamid/cogear/ForgetPasswordActivity.java
+++ app/src/main/java/com/moutamid/cogear/ForgetPasswordActivity.java
@@ -3,12 +3,32 @@ package com.moutamid.cogear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.moutamid.cogear.databinding.ActivityForgetPasswordBinding;
+import com.moutamid.cogear.utilis.Constants;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
+    ActivityForgetPasswordBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forget_password);
+        binding = ActivityForgetPasswordBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnReset.setOnClickListener(v -> {
+            if (binding.email.getEditText().getText().toString().isEmpty()){
+                Toast.makeText(this, "Please Provide a email to reset your password", Toast.LENGTH_SHORT).show();
+            } else {
+                Constants.auth().sendPasswordResetEmail(binding.email.getEditText().getText().toString())
+                        .addOnSuccessListener(unused -> {
+                            Toast.makeText(this, "Please Check Your Email", Toast.LENGTH_SHORT).show();
+                            binding.email.getEditText().setText("");
+                        }).addOnFailureListener(e -> {
+                            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        });
+            }
+        });
     }
 }
